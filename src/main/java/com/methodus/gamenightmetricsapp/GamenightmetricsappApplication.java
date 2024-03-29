fddcf3de80de382c3d370d93c9da1a1f04a1ca7b
TheBababoi <georgekamidis@gmail.com>
2024-03-29 10:03:11 +0200

    Initial commit

---
+++ src/main/java/com/methodus/gamenightmetricsapp/GamenightmetricsappApplication.java
@@ -0,0 +1,13 @@
+package com.methodus.gamenightmetricsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamenightmetricsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamenightmetricsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/methodus/gamenightmetricsapp/GamenightmetricsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.methodus.gamenightmetricsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamenightmetricsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
