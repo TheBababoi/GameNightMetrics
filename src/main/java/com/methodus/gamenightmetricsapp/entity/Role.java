e01ccf3b43fb61685b6c83c42bdf17b56b25e09d
TheBababoi <georgekamidis@gmail.com>
2024-03-30 12:33:17 +0200

    Implemented new Role Entity close #7

---
+++ src/main/java/com/methodus/gamenightmetricsapp/entity/Role.java
@@ -0,0 +1,46 @@
+package com.methodus.gamenightmetricsapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String Name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        Name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", Name='" + Name + '\'' +
+                '}';
+    }
+}
